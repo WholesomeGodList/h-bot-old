07a125360069866df1a5692de9f9936a495a5f0d
Stinggy <wholesomegodlist@gmail.com>
2020-03-25 00:44:27 -0500

    fixed a bug while decrementing searches

--- src/main/java/bot/commands/Search.java
+++ src/main/java/bot/commands/Search.java
@@ -31,107 +31,110 @@ public class Search extends Thread{
     }
 
     public void run() {
-        boolean deepSearch = false;
-        if (args.get(0).equals("deepsearch")){
-            deepSearch = true;
-        }
-        if (args.size() < 2) {
-            waitForDelete(channel.sendMessage("Bruh you're supposed to search something").complete());
-            return;
-        }
-        if(args.get(1).equals("lolicon") || args.get(1).equals("shotacon")){
-            channel.sendMessage("***FBI OPEN UP***").queue();
-            return;
-        }
-        boolean nonrestrict = false;
-        if(args.get(1).equals("-n")) {
-            nonrestrict = true;
-        }
-        PrivateChannel privatechannel = event.getAuthor().openPrivateChannel().complete();
-
-        if(nonrestrict) {
-            privatechannel.sendTyping().complete();
-        }
-        else {
-            channel.sendTyping().complete();
-        }
-
-        waitForDelete(channel.sendMessage(BotAlert.createAlertEmbed("Searching...", "Please wait while the search is being done")).complete());
-
-        StringBuilder queryBuilder = new StringBuilder();
-        if(nonrestrict){
-            for(int i = 2; i < args.size(); i++){
-                queryBuilder.append(args.get(i));
-                queryBuilder.append(" ");
+        try {
+            boolean deepSearch = false;
+            if (args.get(0).equals("deepsearch")) {
+                deepSearch = true;
             }
-        }
-        else{
-            for(int i = 1; i < args.size(); i++){
-                queryBuilder.append(args.get(i));
-                queryBuilder.append(" ");
+            if (args.size() < 2) {
+                hListener.decrementSearches();
+                waitForDelete(channel.sendMessage("Bruh you're supposed to search something").complete());
+                return;
             }
-        }
-        String query = queryBuilder.toString().substring(0, queryBuilder.toString().length() - 1);
-        ArrayList<String> results;
+            if (args.get(1).equals("lolicon") || args.get(1).equals("shotacon")) {
+                hListener.decrementSearches();
+                channel.sendMessage("***FBI OPEN UP***").queue();
+                return;
+            }
+            boolean nonrestrict = false;
+            if (args.get(1).equals("-n")) {
+                nonrestrict = true;
+            }
+            PrivateChannel privatechannel = event.getAuthor().openPrivateChannel().complete();
 
-        if(deepSearch) {
-            results = SoupPitcher.getTopSearchResult(query, 10, nonrestrict);
-        } else {
-            results = SoupPitcher.getTopSearchResult(query, 4, nonrestrict);
-        }
+            if (nonrestrict) {
+                privatechannel.sendTyping().complete();
+            } else {
+                channel.sendTyping().complete();
+            }
 
-        if (results.isEmpty()) {
-            MessageEmbed noResultsAlert = BotAlert.createAlertEmbed("Search Results", "No results found!");
-            if(nonrestrict)
-                privatechannel.sendMessage(noResultsAlert).queue();
-            else
-                channel.sendMessage(noResultsAlert).queue();
-        } else {
-            channel.sendTyping().complete();
-            if(nonrestrict){
-                channel.sendMessage(BotAlert.createAlertEmbed("Non-restrictive mode detected", "Sending the results to your DMs!")).complete();
-            }
-            else {
-                EmbedBuilder bruh = new EmbedBuilder();
-                bruh.setAuthor("Random Search Result", "https://nhentai.net/search/?q=" + query.replaceAll(" ", "+") + "&sort=popular&page=1", "https://i.redd.it/fkg9yip5yyl21.png");
-                bruh.setDescription("Wholesome results all found! Building info embed for random one...");
-                channel.sendMessage(bruh.build()).complete();
-            }
-            try {
-                MessageEmbed eb = InfoBuilder.getInfoEmbed(results.get((int) (Math.random() * results.size())));
-                if(nonrestrict) {
-                    privatechannel.sendMessage(eb).queue();
+            waitForDelete(channel.sendMessage(BotAlert.createAlertEmbed("Searching...", "Please wait while the search is being done")).complete());
+
+            StringBuilder queryBuilder = new StringBuilder();
+            if (nonrestrict) {
+                for (int i = 2; i < args.size(); i++) {
+                    queryBuilder.append(args.get(i));
+                    queryBuilder.append(" ");
                 }
-                else{
-                    channel.sendMessage(eb).queue();
+            } else {
+                for (int i = 1; i < args.size(); i++) {
+                    queryBuilder.append(args.get(i));
+                    queryBuilder.append(" ");
                 }
-            } catch (HttpStatusException e) {
-                logger.info("Exception happened while building random info embed! HTTP status code: " + e.getStatusCode());
+            }
+            String query = queryBuilder.toString().substring(0, queryBuilder.toString().length() - 1);
+            ArrayList<String> results;
+
+            if (deepSearch) {
+                results = SoupPitcher.getTopSearchResult(query, 10, nonrestrict);
+            } else {
+                results = SoupPitcher.getTopSearchResult(query, 4, nonrestrict);
             }
 
-            StringBuilder current = new StringBuilder();
-            current.append("Full results:");
-            while (!results.isEmpty()) {
-                current.append("\n<").append(results.get(0)).append(">");
-                results.remove(0);
-                if (current.length() > 1500) {
-                    if(!nonrestrict) {
-                        channel.sendMessage(current.toString()).queue();
+            if (results.isEmpty()) {
+                MessageEmbed noResultsAlert = BotAlert.createAlertEmbed("Search Results", "No results found!");
+                if (nonrestrict)
+                    privatechannel.sendMessage(noResultsAlert).queue();
+                else
+                    channel.sendMessage(noResultsAlert).queue();
+            } else {
+                channel.sendTyping().complete();
+                if (nonrestrict) {
+                    channel.sendMessage(BotAlert.createAlertEmbed("Non-restrictive mode detected", "Sending the results to your DMs!")).complete();
+                } else {
+                    EmbedBuilder bruh = new EmbedBuilder();
+                    bruh.setAuthor("Random Search Result", "https://nhentai.net/search/?q=" + query.replaceAll(" ", "+") + "&sort=popular&page=1", "https://i.redd.it/fkg9yip5yyl21.png");
+                    bruh.setDescription("Wholesome results all found! Building info embed for random one...");
+                    channel.sendMessage(bruh.build()).complete();
+                }
+                try {
+                    MessageEmbed eb = InfoBuilder.getInfoEmbed(results.get((int) (Math.random() * results.size())));
+                    if (nonrestrict) {
+                        privatechannel.sendMessage(eb).queue();
+                    } else {
+                        channel.sendMessage(eb).queue();
                     }
-                    else {
-                        privatechannel.sendMessage(current.toString()).queue();
+                } catch (HttpStatusException e) {
+                    logger.info("Exception happened while building random info embed! HTTP status code: " + e.getStatusCode());
+                }
+
+                StringBuilder current = new StringBuilder();
+                current.append("Full results:");
+                while (!results.isEmpty()) {
+                    current.append("\n<").append(results.get(0)).append(">");
+                    results.remove(0);
+                    if (current.length() > 1500) {
+                        if (!nonrestrict) {
+                            channel.sendMessage(current.toString()).queue();
+                        } else {
+                            privatechannel.sendMessage(current.toString()).queue();
+                        }
+                        current = new StringBuilder();
                     }
-                    current = new StringBuilder();
+                }
+                if (nonrestrict) {
+                    privatechannel.sendMessage(current.toString()).queue();
+                } else {
+                    channel.sendMessage(current.toString()).queue();
                 }
             }
-            if(nonrestrict){
-                privatechannel.sendMessage(current.toString()).queue();
-            }
-            else {
-                channel.sendMessage(current.toString()).queue();
-            }
+            hListener.decrementSearches();
+            privatechannel.close().queue();
         }
-        hListener.decrementSearches();
-        privatechannel.close().queue();
+        catch (Exception e){
+            logger.info("Something went wrong while searching. Printing stack trace...");
+            e.printStackTrace();
+            hListener.decrementSearches();
+        }
     }
 }
