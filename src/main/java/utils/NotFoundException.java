f9ec98d17873fae8e7e21cb27ab5d46c59135c5e
Stinggy <wholesomegodlist@gmail.com>
2020-04-27 18:04:04 -0500

    testing

--- src/main/java/utils/NotFoundException.java
+++ src/main/java/utils/NotFoundException.java
@@ -1,5 +1,6 @@
 package utils;
 
+//simple wrapper for an exception when i don't find something
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message){
         super(message);
